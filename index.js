4edc022220bd8966acbb3ec445751ecd7279be6e
Enoch Kibet <enochblake9@gmail.com>
2023-12-19 17:32:00 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
-// Declare customerName in global scope
 window.customerName = 'bob';
 
-// Modify customerName to uppercase
 function upperCaseCustomerName() {
   window.customerName = window.customerName.toUpperCase();
 }
 
-// Set bestCustomer to 'not bob'
 window.bestCustomer;
 
 function setBestCustomer() {
   window.bestCustomer = 'not bob';
 }
 
-// Overwrite bestCustomer with a new value
 function overwriteBestCustomer(newBestCustomer) {
   window.bestCustomer = newBestCustomer;
 }
 
-// Attempt to reassign a constant variable (will throw an error)
 const leastFavoriteCustomer = 'someone';
 
 function changeLeastFavoriteCustomer() {
